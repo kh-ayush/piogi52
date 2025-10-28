64902e2f761be98078716c0c9cc884b263cb096b
mrbobrovskiy <bob2ar@yandex.ru>
2025-10-28 22:41:06 +0700

    сделал два класса наверное

--- MainGame/Classes/CEnemy.cs
+++ MainGame/Classes/CEnemy.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace MainGame.Classes
 {
-    public class CEnemy
+    public class Enemy
     {
+        public string Name { get; private set; }
+        public int Lvl { get; private set; }
+        public CBigNum Health { get; private set; }
+        public CBigNum MaxHealth { get; private set; }
+        public CBigNum Reward { get; private set; }
+        public CBigNum Damage { get; private set; }
+        public double HealthModifier { get; private set; }
+        public double RewardModifier { get; private set; }
+
+        public Enemy(string name, int lvl = 1)
+        {
+            Name = name;
+            Lvl = lvl;
+
+            MaxHealth = new CBigNum(50);
+            Health = new CBigNum(50);
+            Damage = new CBigNum(5);
+            Reward = new CBigNum(20);
+
+            HealthModifier = 1.25;
+            RewardModifier = 1.15;
+        }
+
+        public void TakeDamage(CBigNum amount)
+        {
+            if (amount > Health) amount = Health;
+            Health = Health - amount;
+        }
+
+        public bool IsDead()
+        {
+            return Health == new CBigNum(0);
+        }
+
+        public CBigNum GetReward()
+        {
+            return Reward;
+        }
+
+        public void Respawn()
+        {
+            Lvl++;
+            RecalculateStats();
+            Health = MaxHealth;
+        }
+
+        private void RecalculateStats()
+        {
+            MaxHealth = MaxHealth * HealthModifier;
+            Reward = Reward * RewardModifier;
+            Damage = Damage * 1.1;
+        }
     }
 }
--- MainGame/Classes/CPlayer.cs
+++ MainGame/Classes/CPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,67 @@ namespace MainGame.Classes
 {
     public class CPlayer
     {
+        public int Lvl { get; private set; }
+        public CBigNum Gold { get; private set; }
+        public CBigNum Damage { get; private set; }
+        public double DamageModifier { get; private set; }
+        public CBigNum UpgradeCost { get; private set; }
+        public double UpgradeModifier { get; private set; }
+
+        public CPlayer()
+        {
+            Lvl = 1;
+            Gold = new CBigNum(0);
+            Damage = new CBigNum(10);
+            DamageModifier = 1.0;
+            UpgradeCost = new CBigNum(100);
+            UpgradeModifier = 1.15;
+        }
+
+
+        public void AddGold(CBigNum amount)
+        {
+            Gold = Gold + amount;
+        }
+
+        public bool TryUpgrade()
+        {
+            if (TrySpendGold(UpgradeCost))
+            {
+                Lvl++;
+                RecalculateStats();
+                return true;
+            }
+            return false;
+        }
+
+        public bool TrySpendGold(CBigNum amount)
+        {
+            if (Gold > amount || Gold == amount) 
+            {
+                Gold = Gold - amount;
+                return true;
+            }
+            return false;
+        }
+
+        public void RecalculateStats()
+        {
+            Damage = CalculateTotalDamage();
+            UpgradeCost = CalculateNextUpgradeCost();
+        }
+
+        public CBigNum CalculateTotalDamage()
+        {
+            return Damage * (DamageModifier * Lvl);
+        }
+        public CBigNum CalculateNextUpgradeCost()
+        {
+            return UpgradeCost * UpgradeModifier;
+        }
+        public CBigNum DealDamage()
+        {
+            return CalculateTotalDamage();
+        }
     }
 }
