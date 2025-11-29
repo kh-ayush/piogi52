f2e4bbed5d7cfab5674906a1467eea9368b7e988
mrbobrovskiy <bob2ar@yandex.ru>
2025-11-29 15:41:15 +0700

    создал класс красная точка и все

---
+++ MainGame/Classes/CRedObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+namespace MainGame.Classes
+{
+    public class CRedObject : CObject
+    {
+        public CRedObject(System.Drawing.Point position, double size, double lifetime)
+            : base(position, size, lifetime)
+        {
+            Sprite.Fill = Brushes.Red;
+            Sprite.Stroke = Brushes.DarkRed;
+            Sprite.StrokeThickness = 1;
+        }
+    }
+}
